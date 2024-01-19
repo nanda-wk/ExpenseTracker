86d2de943300fa28527d08c1f24fcbfec78c40ee
Nanda WK <nandawinkyu.ix@gmail.com>
2024-01-19 20:44:59 +0630

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Nanda WK on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
